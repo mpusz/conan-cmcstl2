fbad3c869bc9f6e3468ecebe55dd17f60804ddfe
Mateusz Pusz <mateusz.pusz@gmail.com>
2019-04-09 07:06:14 +0200

    Revert "stable_branch_pattern added to build.py"
    This reverts commit cbeba634f8fea57d73603a6890b93c0488f7aff5.

--- build.py
+++ build.py
@@ -2,7 +2,6 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="mpusz", login_username="mpusz",
-                                 stable_branch_pattern=["master$", "release*", "stable*", "v*"],
                                  channel="testing",
                                  upload="https://api.bintray.com/conan/mpusz/conan-mpusz")
     builder.add_common_builds(pure_c=False)
